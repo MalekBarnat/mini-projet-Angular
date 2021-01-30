9a6d72787d6ffcb8846f920957d2a2b3fc7d487e
MalekBarnat <barnatmalek@protonmail.com>
2021-01-30 23:41:03 +0100

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pageId:number=0;
   title = 'MalekSiwar';
+  loadpage(pageId:number){
+    this.pageId=pageId;
+  }
 }
